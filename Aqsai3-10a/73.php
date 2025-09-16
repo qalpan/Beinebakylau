7466e99169369bad86c66d333f0127615c518380
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-09-16 14:05:06 +0500

    Add files via upload

---
+++ Aqsai3-10a/73.php
@@ -0,0 +1,11 @@
+<?php
+include 'basy.php';
+?>
+<body>
+<div id="basty">
+<div class="kөrme2">
+<a><img src="sůuretter/73a.webp" title="73a" alt="73a" data-image="sůuretter/73a.webp" data-description="73a"></a>
+<a><img src="sůuretter/habarlandyryu.webp" title="habarlandyryu" alt="habarlandyryu" data-image="sůuretter/habarlandyryu.webp" data-description="habarlandyryu"></a>
+</div>
+</div>
+</body>
---
+++ Aqsai3-10a/basy.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">     
+<link rel="shortcut icon" href="sůuretter/taꞑba-aqsai3-10a.svg" type="image/svg">
+<title>Aqsai3-10a - ůi býrlestýgý</title>
+<link rel='stylesheet' href='yi.css' type='text/css'/> 
+
+</head>
+
+<script type="text/javascript">
+document.ondragstart = test;
+document.onselectstart = test;
+document.oncontextmenu = test;
+function test(){return false}
+</script>
